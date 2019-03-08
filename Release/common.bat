94d29fa80b317ac3ec062a3d8a24497e49409680
Konstantin Nosov <git@gildor.org>
2019-03-08 13:37:31 +0300

    Build tools: - updated "build-common": it wasn't able to run SymInfoBuilder, it seems it requires now to point at location relative to current bash script - copying pdb file in common.bat

--- Release/common.bat
+++ Release/common.bat
@@ -3,5 +3,6 @@ set QDIR=\games\quake2
 copy /b *.exe %QDIR%\*.* >NUL
 if exist *.dll copy /b *.dll %QDIR%\*.* >NUL
 if exist *.dbg copy /b *.dbg %QDIR%\*.* > NUL
+if exist *.pdb copy /b *.pdb %QDIR%\*.* > NUL
 copy /b ..\4.XX_Changes.txt %QDIR%\*.* >NUL
 cd %QDIR%
